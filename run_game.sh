bc5ed218906018f0519e97744ca5e5a7424703bb
Älva Läckgren <deadhillbill123@gmail.com>
2023-11-14 20:50:42 +0100

    inserted into script - outputs, compiler, game runner and then removing class files

--- run_game.sh
+++ run_game.sh
@@ -7,3 +7,18 @@ mkdir Ealva_Leackgren_labb
 cp GuessingGame.java Guesser.java Ealva_Leackgren_labb
 cd Ealva_Leackgren_labb
 pwd
+
+CURRENT_DIR=$(pwd)
+echo "Running game from $CURRENT_DIR"
+
+echo "compiling..."
+javac GuessingGame.java
+
+echo "running..."
+java GuessingGame
+
+echo "Done"
+
+echo "removing class files..."
+rm *.class
+ls
