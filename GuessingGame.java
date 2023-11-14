8caf3d3b5b85494e926a5d135812472a4ee87e4c
Älva Läckgren <deadhillbill123@gmail.com>
2023-11-14 17:04:31 +0100

    coed in main class changed

--- GuessingGame.java
+++ GuessingGame.java
@@ -1,6 +1,6 @@
 public class GuessingGame {
     public static void main(String[] args) {
-        Guesser guess1 = new Guesser(0, 1000);
+        Guesser guess1 = new Guesser(0, 10);
         guess1.start();
     }
 }
